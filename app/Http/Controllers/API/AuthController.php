4cd5f4dbbacf5927ac7e1d6c9241ccff87e5a844
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 16:29:36 +0400

    Make AuthController class.

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *  app/Http/Controllers/API/AuthController.php
+ *
+ * Date-Time: 02.06.21
+ * Time: 16:28
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * Class AuthController
+ * @package App\Http\Controllers\API
+ */
+class AuthController extends Controller
+{
+    //
+}
