eb1bf13f7166927cd1141f3ac99e0a21836e85b6
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 16:29:14 +0400

    Make middleware auth sanctum.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/',function (Request $request) {
+        return $request->user();
+    });
 });
